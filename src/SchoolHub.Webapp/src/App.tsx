a1fb66184f24ccefd135c3db7c8933809990f376
007anthony <anthony.pena@lernende.bbw.ch>
2023-12-31 17:51:13 +0100

    add routing

--- src/SchoolHub.Webapp/src/App.tsx
+++ src/SchoolHub.Webapp/src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
 function App() {
   return (
-    <></>
+    <BrowserRouter>
+		<Routes>
+			<Route path='/' element={<Home/>}/>
+		</Routes>
+    </BrowserRouter>
   );
 }
 
